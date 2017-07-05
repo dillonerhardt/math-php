c6deb15d0ee64f326270d768532a201e577c15cd
dillonerhardt <dillon@erhardt.co.uk>
2017-07-05 08:18:43 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+PHP Maths Library
+==========
+
+[![Minimum PHP Version](https://img.shields.io/badge/php-%3E%3D%207.0-8892BF.svg)](https://php.net/)
+
+A simple library of more interesting and complex maths functions in PHP7
+
+##Features
---
+++ src/Math/Matrix.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * Matrix Class
+ */
+class Matrix {
+    /**
+     * @var int
+     */
+    private $rows;
+
+    /**
+     * @var int
+     */
+    private $cols;
+
+    /**
+     * @var array
+     */
+    private $matrix;
+
+    /**
+     * Constructor method to initialize matrix
+     *
+     * @param array $matrix
+     */
+    function __construct(array $matrix) {
+        // Set matrix rows
+        $this->rows = count($matrix);
+        // Set matric columns
+        $this->cols = count($matrix[0]);
+        // Set matrix
+        $this->matrix = $matrix;
+    }
+
+    /**
+     * Add method
+     *
+     * @param $num
+     * @return void
+     */
+    public function add($n) {
+        if($n instanceof Matrix) {
+            for ($i = 0; $i < $this->rows; $i++) { 
+                for ($j = 0; $j < $this->cols; $j++) { 
+                    $this->matrix[$i][$j] += $n->matrix[$i][$j];
+                }
+            }
+        } else {
+            for ($i = 0; $i < $this->rows; $i++) { 
+                for ($j = 0; $j < $this->cols; $j++) { 
+                    $this->matrix[$i][$j] += $n;
+                }
+            }
+        }
+    }
+
+    /**
+     * Array of matrix
+     * 
+     * @return array
+     */
+    public function toArray() : array {
+        return $this->matrix;
+    }
+
+    /**
+     * @return int
+     */
+    function getRows() : int {
+        return $this->rows;
+    }
+
+    /**
+     * @return int
+     */
+    function getColumns() : int {
+        return $this->cols;
+    }
+
+    /**
+     * Get values from column of matrix
+     *
+     * @param int $col
+     * @return array
+     */
+    function getColumnValues(int $col) : array {
+        return array_column($this->matrix, $col);
+    } 
+
+}
